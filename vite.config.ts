2beae7bcbd18caa3b1d97b479a6a4047d775058c
matthew1115 <matthew_1115@outlook.com>
2025-07-22 19:40:28 +0800

    tailwind and shadcnui stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   // Set the base path for deployment
-  base: '/OpenLexicon/', 
+  base: '/OpenLexicon/',
 })
